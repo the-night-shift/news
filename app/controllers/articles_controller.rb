2a87d7219f99bbd021ede47480e9766a34847a60
Brian <riceb53@gmail.com>
2018-03-27 18:41:34 -0500

    add secured api key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = Unirest.get("https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=b4a69789b6cf41128d999af0122111b4")
+    response = Unirest.get("https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=#{ENV['API_KEY']}")
     render json: response.body
   end
 end
--- frontend.rb
+++ frontend.rb
@@ -1,5 +1,3 @@
 require 'unirest'
-
 response = Unirest.get("localhost:3000/articles")
-
 p response.body
